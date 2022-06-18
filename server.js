758a704663cba50725a76a46f4813680f3ba39c7
Arnab Chakraborty <arnab@Arnabs-MacBook-Air.local>
2022-06-18 19:17:05 +0530

    project name error solved

--- server.js
+++ server.js
@@ -1,8 +1,8 @@
 const express = require('express');
 const path = require('path');
 const app = express();
-app.use(express.static(__dirname + '/dist/the-final-movie-project'));
+app.use(express.static(__dirname + '/dist/movie-engine-01'));
 app.get('/*', function(req,res) {
 res.sendFile(path.join(__dirname+
-'/dist/the-final-movie-project/index.html'));});
+'/dist/movie-engine-01/index.html'));});
 app.listen(process.env.PORT || 8080);
